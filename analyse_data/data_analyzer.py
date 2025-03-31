35f3d3f9ae5d4fb613ce0f6af872d86b79349c43
szymsta <stanislawszymanski89@gmail.com>
2025-03-31 20:48:20 +0200

    Add function to calculate unique ships by country flag in data_analyzer.py

--- analyse_data/data_analyzer.py
+++ analyse_data/data_analyzer.py
@@ -12,10 +12,14 @@ class AnalyzeData:
     Constants:
         DYNAMIC_MSG_TYPES (list): A list containing the valid message types to filter by.
         MESSAGE_TYPE (str): The column name for the message type.
+        COUNTRY (str): The column name for the country.
+        MMSI (str): The column name for the Maritime Mobile Service Identity (unique ship identifier).
     """
 
     DYNAMIC_MSG_TYPES : list[int] = [1, 2, 3, 18, 19]
     MESSAGE_TYPE = "msg_type"
+    COUNTRY = "country"
+    MMSI = "mmsi"
 
 
     def __init__(self, spark_session: SparkSession):
@@ -40,3 +44,25 @@ class AnalyzeData:
             DataFrame: A filtered DataFrame containing only rows with valid message types.
         """
         return  df.filter(col(self.MESSAGE_TYPE).isin(self.DYNAMIC_MSG_TYPES))
+
+
+    def calculate_country(self, df: DataFrame) -> DataFrame:
+        """
+        Groups the DataFrame by the country column, counts the number of unique ships 
+        (identified by MMSI) for each country, and sorts the results in descending order 
+        of the count of unique ships.
+
+        Args:
+            df (DataFrame): The DataFrame containing the data to be analyzed, which should include a 
+                            column for country information and MMSI (Maritime Mobile Service Identity) 
+                            to identify unique ships.
+
+        Returns:
+            DataFrame: A DataFrame with the countries grouped by their flag, showing the count of unique 
+                    ships (MMSI), sorted in descending order by the count of unique ships.
+        """
+        return (df.dropDuplicates([self.MMSI])
+                .groupBy(col(self.COUNTRY))
+                .count()
+                .sort("count", ascending=False)
+        )
