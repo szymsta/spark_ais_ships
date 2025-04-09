0ffd14e5fa7ce686c45f7d12f30c26fdb1d0e5f3
szymsta <stanislawszymanski89@gmail.com>
2025-04-09 19:18:29 +0200

    Update variables in config.py for project.

--- config.py
+++ config.py
@@ -18,16 +18,25 @@ class Config:
         LOG_LEVEL (int): The log level to be used (e.g., INFO, DEBUG, ERROR).
         LOG_FORMAT (str): The format string for log messages.
         LOG_HANDLERS (list): The handlers for logging output (file and console).
-        MAP_OUTPUT_FILE (str): The output file name and path for the map visualization.
+        TARGET_MMSI (int): A single target MMSI number used for filtering specific ships.
+        TARGET_MMSI_LIST (list): A list of MMSI numbers used to search for multiple specific ships.
+        LAT_MIN (float): Minimum latitude value for location-based filtering.
+        LAT_MAX (float): Maximum latitude value for location-based filtering.
+        LON_MIN (float): Minimum longitude value for location-based filtering.
+        LON_MAX (float): Maximum longitude value for location-based filtering.
+        COUNTRY_FLAG (str): Country name used to filter ships by their registration flag.
+        MAP_OUTPUT_FILE_1 (str): Output file path for the single ship map visualization.
+        MAP_OUTPUT_FILE_2 (str): Output file path for the country-flag ship map visualization.
+        MAP_OUTPUT_FILE_3 (str): Output file path for the multiple selected ships map visualization.
         FILE_NAMES (list): List of CSV file names to load.
         FILE_FORMAT (str): The format of the files to be loaded.
         FILE_OPTIONS (dict): CSV loading options such as header, schema inference, and delimiter.
         DISTANCE_UNIT (Unit): The unit for measuring distance (e.g., kilometers).
-        DYNAMIC_MSG_TYPES (list): List of message types associated with dynamic data.
+        DYNAMIC_MSG_TYPES (list): List of message types associated with dynamic AIS data.
         SPEED_CONVERSION_FACTOR (float): Conversion factor for speed from knots to kilometers per hour.
         MMSI_VALID_PREFIX_PATTERN (str): Pattern for valid ship MMSI numbers starting with digits 2–7.
         MAP_ZOOM (int): Default zoom level for map visualization.
-        MAP_HEIGHT (int): Default height for map visualization.
+        MAP_HEIGHT (int): Default height for map visualization in pixels.
         MAP_STYLE (str): The style of the map visualization (e.g., "open-street-map").
     """
 
@@ -46,14 +55,17 @@ class Config:
     LOG_HANDLERS = [logging.FileHandler(LOG_FILE), logging.StreamHandler()] # Default handlers: to a file and to the console
     
     # Search data parameters:
-    TARGET_MMSI = 319205600                     # MMSI number for searching a single ship
-    TARGET_MMSI_LIST = [319205600, 257988000]   # List of MMSI numbers for searching multiple ships
-    LAT_MIN, LAT_MAX = 57.0, 59.0               # Latitude range for searching ships by location
-    LON_MIN, LON_MAX = 4.0, 5.4                 # Longitude range for searching ships by location
-    COUNTRY_FLAG = "Poland (Republic of)"       # Country flag for searching ships by their country of registration
+    TARGET_MMSI = 477327900                                         # MMSI number for searching a single ship
+    TARGET_MMSI_LIST = [477327900, 371861000, 247229700, 309075000,
+                        355931000, 256281000, 249652000]            # List of MMSI numbers for searching multiple ships
+    LAT_MIN, LAT_MAX = 56.0, 61.0                                   # Latitude range for searching ships by location
+    LON_MIN, LON_MAX = 9.0, 11.0                                    # Longitude range for searching ships by location
+    COUNTRY_FLAG = "Norway"                                         # Country flag for searching ships by their country of registration
 
     # map name & path - main_app.py
-    MAP_OUTPUT_FILE = "ships_map.html"
+    MAP_OUTPUT_FILE_1 = "route_single_ship_map.html"
+    MAP_OUTPUT_FILE_2 = "route_ships_country_map.html"
+    MAP_OUTPUT_FILE_3 = "route_selected_ships_map.html"
 
 
     # File name configuration - data_loader.py
