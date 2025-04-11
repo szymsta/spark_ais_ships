1b81fbc9eb4aa15774c474fa715826104bb609d7
szymsta <stanislawszymanski89@gmail.com>
2025-04-11 14:59:36 +0200

    Add row_data function as fixture for loading raw data to the DafaFrame.

--- tests/test_load_data/test_data_loader.py
+++ tests/test_load_data/test_data_loader.py
@@ -4,6 +4,20 @@ from tests.spark_session_test import spark
 from pyspark.sql.functions import length
 
 
+# Prepare function as fixture
+@pytest.fixture
+def raw_data(spark):
+    """
+    Fixture to load raw data from the 'test_file.csv' using LoadData class.
+
+    Returns:
+        DataFrame: Raw data loaded as a Spark DataFrame.
+    """
+    loader = LoadData(spark)
+    return loader.load_datasets("test_file.csv")
+
+
+# Test spark session
 def test_spark_session_creation(spark):
     """
     Test that the Spark session is successfully created.
@@ -15,35 +29,23 @@ def test_spark_session_creation(spark):
     assert spark is not None
     assert spark.version is not None
 
-def test_create_mid(spark):
-    """
-    This test ensures that the 'mid' column is properly generated from the MMSI column,
-    and verifies that the data is correctly loaded from the CSV file.
 
-    Steps:
-        1. Load the dataset using the LoadData class.
-        2. Check that the 'mid' column exists in the DataFrame.
-        3. Ensure that at least some data is loaded (15 records).
-        4. Validate that there is one row where the 'mid' value has provided value.
+# Test load function
+def test_create_mid(spark, raw_data):
+    """
+    This test ensures that the 'mid' column is properly generated from the MMSI column.
 
     Args:
         spark (SparkSession): The Spark session used to run the test.
 
-    Asserts:
+    Verifies:
         - 'mid' column exists in the loaded DataFrame.
         - 15 rows are loaded from the CSV file.
         - one row with 'mid' equal to 232.
         - 'mid' values have exactly 3 characters.
     """
-    # Assign the class to constant
-    loader = LoadData(spark)
-
-    # Create DataFrame
-    ais_df = loader.load_datasets("test_file.csv")
-
-    # Filter rows where the length of 'mid' (cast to string) is not equal to 3
-    mid_length_count = ais_df.filter(
-        (length(ais_df["mid"].cast("string")) != 3)).count()
+    # Create DF
+    ais_df = raw_data
 
     # Test 1: Check if 'mid' column exists and correct number of rows is loaded
     assert "mid" in ais_df.columns  # The 'mid' column should exist
@@ -53,4 +55,5 @@ def test_create_mid(spark):
     assert ais_df.filter(ais_df["mid"] == 232).count() >= 1 # At least one match expected
     
     # Test 3: Ensure all 'mid' values have exactly 3 characters
-    assert mid_length_count == 0
+    mid_length_count = ais_df.filter((length(ais_df["mid"].cast("string")) != 3)).count()
+    assert mid_length_count == 0    # Should be 0
